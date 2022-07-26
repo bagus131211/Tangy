c2c03f24beff493b930a202a8fa2a029e355463d
Bagus Ramadhan Susetyo <b.susetyo@creditinfo.com>
2022-07-26 15:11:02 +0700

    commit Capture all values with splatting

---
+++ Tangy.Models/LearnBlazorModels/Demo.Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Tangy.Models.LearnBlazorModels
+{
+    public class DemoProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+        public ICollection<DemoProductProperties> ProductProperties { get; set; }
+
+    }
+}
