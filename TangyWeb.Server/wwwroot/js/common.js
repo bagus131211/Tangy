f55c9693baa639bb044b0a58798fc3f9e4c22d68
Bagus Ramadhan Susetyo <b.susetyo@creditinfo.com>
2022-07-26 20:38:54 +0700

    commit case 52. Add helper extension method to js runtime

--- TangyWeb.Server/wwwroot/js/common.js
+++ TangyWeb.Server/wwwroot/js/common.js
@@ -2,5 +2,5 @@
     if (type === "success")
         toastr.success(message, 'Operation successful', { timeOut: 2000 });
     else if (type === "error")
-        toastr.error(message, 'Operation successful', { timeOut: 2000 });
+        toastr.error(message, 'Operation failed', { timeOut: 2000 });
 }
