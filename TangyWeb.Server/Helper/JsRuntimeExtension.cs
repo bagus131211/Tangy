5e57ffedecad651cb83665b17667a5473a1228c6
Bagus Ramadhan Susetyo <b.susetyo@creditinfo.com>
2022-07-26 20:38:54 +0700

    commit case 52. Add helper extension method to js runtime

---
+++ TangyWeb.Server/Helper/JsRuntimeExtension.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.JSInterop;
+
+namespace TangyWeb.Server.Helper
+{
+    public static class JsRuntimeExtension
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message) 
+            => await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+
+        public static async ValueTask ToastrFailure(this IJSRuntime jsRuntime, string message)
+            => await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+
+    }
+}
