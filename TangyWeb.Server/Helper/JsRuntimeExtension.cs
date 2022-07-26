f6165704e776626e6ce7c9b582d59a22664828a0
Bagus Ramadhan Susetyo <b.susetyo@creditinfo.com>
2022-07-26 21:17:28 +0700

    commit case 53. Assignment to integrate with SweetAlert js

--- TangyWeb.Server/Helper/JsRuntimeExtension.cs
+++ TangyWeb.Server/Helper/JsRuntimeExtension.cs
@@ -10,5 +10,11 @@ namespace TangyWeb.Server.Helper
         public static async ValueTask ToastrFailure(this IJSRuntime jsRuntime, string message)
             => await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
 
+        public static async ValueTask SwalSuccess(this IJSRuntime jsRuntime, string message)
+            => await jsRuntime.InvokeVoidAsync("ShowSwal", "success", message);
+
+        public static async ValueTask SwalFailure(this IJSRuntime jsRuntime, string message)
+            => await jsRuntime.InvokeVoidAsync("ShowSwal", "error", message);
+
     }
 }
--- TangyWeb.Server/wwwroot/js/common.js
+++ TangyWeb.Server/wwwroot/js/common.js
@@ -3,4 +3,11 @@
         toastr.success(message, 'Operation successful', { timeOut: 2000 });
     else if (type === "error")
         toastr.error(message, 'Operation failed', { timeOut: 2000 });
+};
+
+window.ShowSwal = (type, message) => {
+    if (type === "success")
+        Swal.fire("Success Notification!", message, "success");
+    if (type === "error")
+        Swal.fire("Error Notification!!!", message, "error");
 }
