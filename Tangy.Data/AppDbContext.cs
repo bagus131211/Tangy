dc5b166f47979c1253bbb20535a3b34b2947c5f5
Bagus Ramadhan Susetyo <b.susetyo@creditinfo.com>
2022-07-27 17:06:31 +0700

    commit case 61 - 69. Model and Repository

---
+++ Tangy.Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tangy.Data.Models;
+
+namespace Tangy.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opts) : base(opts)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
