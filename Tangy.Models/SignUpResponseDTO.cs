44342f9380ae1b0c1803e13bce151c9c50f6b950
Bagus Ramadhan Susetyo <b.susetyo@creditinfo.com>
2022-08-03 21:50:39 +0700

    commit case 163-173. Add Authorization in the TangyWeb.API

---
+++ Tangy.Models/SignUpResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Tangy.Models
+{
+    public class SignUpResponseDTO
+    {
+        public bool IsSignUpSuccessful { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
---
+++ Tangy.Models/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Tangy.Models
+{
+    public class UserDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
